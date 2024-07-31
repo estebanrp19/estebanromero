3037ebaee7db3cfdeec6262e3e8bf4a279847966
estebanrp19 <esteban.r1905@gmail.com>
2024-07-31 16:34:23 -0500

    cambios

---
+++ src/app/aboutme/page.tsx
@@ -0,0 +1,9 @@
+import About from "@/components/About";
+
+function AboutPage () {
+    return (
+        <About /> 
+    )
+}
+
+export default AboutPage;
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,14 +4,14 @@ function Navbar () {
     return (
       <div>
         <nav className="flex justify-between navbar bg-[rgba(255,255,255,.4)] mx-20 my-5 p-4 rounded-[40px]">
-          <Link href="#">
+          <Link href="/">
             <img
               src="/home.svg"
               alt=""
               className="mx-20 filter invert hover:bg-[#fa7d35] p-1.5 rounded active:bg-[#fa7d35]"
             />
           </Link>
-          <Link href="#">
+          <Link href="/aboutme">
             <img
               src="/user-male.svg"
               alt=""
