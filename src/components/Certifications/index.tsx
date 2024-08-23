496c7bc28d638b4a4ba06b148e88c18a199dfb9a
estebanrp19 <esteban.r1905@gmail.com>
2024-08-23 16:17:36 -0500

    Se añadio el certificado de Soy henry al componente skills

--- src/components/Certifications/index.tsx
+++ src/components/Certifications/index.tsx
@@ -2,8 +2,8 @@ function Certifications() {
   return (
     <div className="mt-10 text-center">
       <h1 className="text-[25px] text-[#A3AEC2] font-semibold">Certificados</h1>
-      <div className="flex justify-center">
-        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-[#F2F5FC] p- m-4">
+      <div className="flex flex-wrap justify-center gap-4 mt-4">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-[#F2F5FC] p-4">
           <img
             src="/certificado-diseño-web.jpg"
             alt="Certificado de Diseño Web"
@@ -14,6 +14,25 @@ function Certifications() {
           </div>
           <div className="mb-2">
             <a href="/CERTIFICADO-CURSO-DISEÑO-WEB.pdf" download>
+              <button className="px-3 py-2 bg-[#A3AEC2] text-white rounded-lg hover:bg-white hover:text-[#A3AEC2]">
+                Descargar
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-[#F2F5FC] p-4">
+          <img
+            src="/CERTIFICADO_SOY_HENRY_FULL_STACK.png"
+            alt="Certificado de Desarrollo Web Full Stack"
+            className="w-full h-auto rounded"
+          />
+          <div className="px-6 py-4">
+            <p className="text-[#364655] text-base">
+              Desarrollo Web Full Stack
+            </p>
+          </div>
+          <div className="mb-2">
+            <a href="/CERTIFICADO_SOY_HENRY_FULL_STACK.png" download>
               <button className="px-3 py-2 bg-[#A3AEC2] text-white rounded-lg hover:bg-white hover:text-[#A3AEC2]">
                 Descargar
               </button>
